84823bdc6ab51c2506f465da86ea108a97d1c990
Bekir Akıl <78118245+tinydeveloper-bkr@users.noreply.github.com>
2025-08-25 00:00:26 +0300

    Update and rename omni_r9s.mk to twrp_r9s.mk

--- twrp_r9s.mk
+++ twrp_r9s.mk
@@ -7,23 +7,25 @@
 
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
 
-# Inherit some common Omni stuff.
-$(call inherit-product, vendor/omni/config/common.mk)
+# Enable project quotas and casefolding for emulated storage without sdcardfs
+$(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
+
+# Inherit some common TWRP stuff.
+$(call inherit-product, vendor/twrp/config/common.mk)
 
 # Inherit from r9s device
 $(call inherit-product, device/samsung/r9s/device.mk)
 
 PRODUCT_DEVICE := r9s
-PRODUCT_NAME := omni_r9s
+PRODUCT_NAME := twrp_r9s
 PRODUCT_BRAND := samsung
 PRODUCT_MODEL := SM-G990E
 PRODUCT_MANUFACTURER := samsung
-
 PRODUCT_GMS_CLIENTID_BASE := android-samsung-ss
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="r9sxxx-user 11 RP1A.200720.012 G990EXXS7EWG1 release-keys"
+    PRIVATE_BUILD_DESC="r9sxxx-user 11 RP1A.200720.012 G990EXXSHGYH2 release-keys"
 
-BUILD_FINGERPRINT := samsung/r9sxxx/r9s:11/RP1A.200720.012/G990EXXS7EWG1:user/release-keys
+BUILD_FINGERPRINT := samsung/r9sxxx/r9s:11/RP1A.200720.012/G990EXXSHGYH2:user/release-keys
