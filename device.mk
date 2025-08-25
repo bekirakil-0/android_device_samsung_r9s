73bac40f5c1ec9bc374b6529697e7e014b295ead
Bekir Akıl <78118245+tinydeveloper-bkr@users.noreply.github.com>
2025-08-25 03:21:09 +0300

    Update device.mk

--- device.mk
+++ device.mk
@@ -6,3 +6,17 @@
 #
 
 LOCAL_PATH := device/samsung/r9s
+
+# Dynamic partitions
+PRODUCT_USE_DYNAMIC_PARTITIONS := true
+
+# fastbootd
+PRODUCT_PACKAGES += \
+    android.hardware.fastboot@1.0-impl-mock \
+    fastbootd
+
+# device decryption (need fix)
+#PRODUCT_PACKAGES += \ (product packages needed for decryption)
+
+# Enable Fuse Passthrough
+PRODUCT_PROPERTY_OVERRIDES += persist.sys.fuse.passthrough.enable=true
