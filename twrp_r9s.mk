b39895b5cd547c460ef716c2b3c7bda5ff6b3738
bekirakil-0 <228974116+bekirakil-0@users.noreply.github.com>
2025-10-30 19:43:44 +0300

    update

--- twrp_r9s.mk
+++ twrp_r9s.mk
@@ -1,13 +1,27 @@
 #
 # Copyright (C) 2023 The Android Open Source Project
-# Copyright (C) 2023 Bekirakil
 #
 # SPDX-License-Identifier: Apache-2.0
 #
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
 
-# Inherit from those products. Most specific first.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+# Release name
+PRODUCT_RELEASE_NAME := r9s
+
+# Inherit from common AOSP config
 $(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 
 # Enable project quotas and casefolding for emulated storage without sdcardfs
 $(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
@@ -15,7 +29,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
 # Inherit some common TWRP stuff.
 $(call inherit-product, vendor/twrp/config/common.mk)
 
-# Inherit from r9s device
+# Inherit device configuration
 $(call inherit-product, device/samsung/r9s/device.mk)
 
 # Charger
@@ -24,17 +38,10 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,device/samsung/r9s/recovery/root,recovery/root)
 
-#PRODUCT_BUILD_PROP_OVERRIDES += \
-    #PRIVATE_BUILD_DESC="r9sxxx-user 11 RP1A.200720.012 G990EXXSHGYH2 release-keys"
-
-#BUILD_FINGERPRINT := samsung/r9sxxx/r9s:11/RP1A.200720.012/G990EXXSHGYH2:user/release-keys
-
-
-## Device identifier
-PRODUCT_DEVICE := r9s
+## Device identifier. This must come after all inclusions
 PRODUCT_NAME := twrp_r9s
-PRODUCT_BRAND := samsung
+PRODUCT_DEVICE := r9s
 PRODUCT_MODEL := SM-G990E
+PRODUCT_BRAND := samsung
 PRODUCT_MANUFACTURER := samsung
-PRODUCT_GMS_CLIENTID_BASE := android-samsung
-
+PRODUCT_GMS_CLIENTID_BASE := android-samsung-ss
--- vendorsetup.sh
+++
@@ -1,7 +0,0 @@
-#
-# Copyright (C) 2023 The Android Open Source Project
-# Copyright (C) 2023 tinydeveloper-bkr
-#
-# SPDX-License-Identifier: Apache-2.0
-#
-add_lunch_combo twrp_r9s-eng
